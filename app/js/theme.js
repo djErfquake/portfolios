1635b9f45c67b16c256cfde313aa47b1a639e77d
Calvin <calvinlevally@gmail.com>
2019-08-29 16:16:33 -0400

    projects working better

--- app/js/theme.js
+++ app/js/theme.js
@@ -105,29 +105,29 @@
 	
 	$('select').niceSelect();
 	
-	/*----------------------------------------------------*/
-    /*  Simple LightBox js
-    /*----------------------------------------------------*/
-    $('.imageGallery1 .light').simpleLightbox();
+	// /*----------------------------------------------------*/
+    // /*  Simple LightBox js
+    // /*----------------------------------------------------*/
+    // $('.imageGallery1 .light').simpleLightbox();
 	
-	$('.counter').counterUp({
-		delay: 10,
-		time: 1000
-	});
+	// $('.counter').counterUp({
+	// 	delay: 10,
+	// 	time: 1000
+	// });
 	
 	
-	$(".skill_main").each(function() {
-        $(this).waypoint(function() {
-            var progressBar = $(".progress-bar");
-            progressBar.each(function(indx){
-                $(this).css("width", $(this).attr("aria-valuenow") + "%")
-            })
-        }, {
-            triggerOnce: true,
-            offset: 'bottom-in-view'
+	// $(".skill_main").each(function() {
+    //     $(this).waypoint(function() {
+    //         var progressBar = $(".progress-bar");
+    //         progressBar.each(function(indx){
+    //             $(this).css("width", $(this).attr("aria-valuenow") + "%")
+    //         })
+    //     }, {
+    //         triggerOnce: true,
+    //         offset: 'bottom-in-view'
 
-        });
-    });
+    //     });
+    // });
 	
 	/*----------------------------------------------------*/
     /*  Isotope Fillter js
